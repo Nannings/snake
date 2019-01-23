66fe0177c82952682b21864d5ccfd2b1b386f374
Nannings <mrnannings@gmail.com>
2019-01-23 15:02:39 +0100

    2;

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,13 +11,19 @@ namespace SA
 
         public Color color1;
         public Color color2;
+        public Color playerColor = Color.black;
+
+        GameObject playerObj;
 
         GameObject mapObject;
         SpriteRenderer mapRenderer;
 
+        Node[,] grid;
+
         private void Start()
         {
             CreateMap();
+            PlacePlayer();
         }
 
         void CreateMap()
@@ -25,11 +31,25 @@ namespace SA
             mapObject = new GameObject("Map");
             mapRenderer = mapObject.AddComponent<SpriteRenderer>();
 
+            grid = new Node[maxWidth, maxHeight];
+
             Texture2D txt = new Texture2D(maxWidth, maxHeight);
             for (int x = 0; x < maxWidth; x++)
             {
                 for (int y = 0; y < maxHeight; y++)
                 {
+                    Vector3 tp = Vector3.zero;
+                    tp.x = x;
+                    tp.y = y;
+
+                    Node n = new Node()
+                    {
+                        x = x,
+                        y = y,
+                        worldPosition = tp
+                    };
+                    grid[x, y] = n;
+
                     if (x % 2 != 0)
                     {
                         if (y % 2 != 0)
@@ -60,6 +80,34 @@ namespace SA
             Rect rect = new Rect(0, 0, maxWidth, maxHeight);
             Sprite sprite = Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
             mapRenderer.sprite = sprite;
+        }
+
+        void PlacePlayer()
+        {
+            playerObj = new GameObject("Player");
+            SpriteRenderer playerRender = playerObj.AddComponent<SpriteRenderer>();
+            playerRender.sprite = CreateSprite(playerColor);
+            playerRender.sortingOrder = 1;
+            playerObj.transform.position = GetNode(3, 3).worldPosition;
+        }
+
+        Node GetNode(int x, int y)
+        {
+            if (x < 0 || x > maxWidth - 1 || y < 0 || y > maxHeight - 1)
+            {
+                return null;
+            }
+            return grid[x, y];
+        }
+
+        Sprite CreateSprite(Color targetColor)
+        {
+            Texture2D txt = new Texture2D(1, 1);
+            txt.SetPixel(0, 0, targetColor);
+            txt.filterMode = FilterMode.Point;
+            txt.Apply();
+            Rect rect = new Rect(0, 0, 1, 1);
+            return Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
         }
     }
 }
